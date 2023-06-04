f49a8b5206fb3a213a4fc56e8127163cb91edc73
imamuray <imamuray1313@gmail.com>
2023-06-04 00:52:19 +0000

    index_test修正

--- index_test.go
+++ index_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -9,6 +9,21 @@ func TestIndexAdd(t *testing.T) {
 	idx := make(index)
 	idx.add([]document{{ID: 1, Text: "A donut on a glass plate. Only the donuts."}})
 	idx.add([]document{{ID: 2, Text: "donut is a donut"}})
-	// map[string][]intの比較がめんどいのでパス
-	fmt.Println(idx)
+
+	want := map[string][]int{
+		"donut": {1, 2}, "donuts": {1}, "glass": {1}, "is": {2}, "on": {1}, "only": {1},
+	}
+
+	for key, value := range want {
+		t.Run("indexへの追加テスト", func(t *testing.T) {
+			if got, ok := idx[key]; !ok {
+				t.Errorf("idx[%v] dosen't exist.", key)
+			} else {
+				if !reflect.DeepEqual(got, value) {
+					t.Errorf("idx[%v] = %v, want %v", key, got, value)
+				}
+			}
+		})
+	}
+
 }
