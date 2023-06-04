f5801f1d430b55c4c4e3118c4be79324084f0f18
imamuray <imamuray1313@gmail.com>
2023-06-04 00:14:56 +0000

    document.go作成

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
 	"os"
 	"regexp"
@@ -9,35 +8,6 @@ import (
 	"time"
 	"unicode"
 )
-
-type document struct {
-	Title string `xml:"title"`
-	URL   string `xml:"url"`
-	Text  string `xml:"abstract"`
-	ID    int
-}
-
-func loadDocuments(path string) ([]document, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	dec := xml.NewDecoder(f)
-	dump := struct {
-		Documents []document `xml:"doc"`
-	}{}
-	if err := dec.Decode(&dump); err != nil {
-		return nil, err
-	}
-
-	docs := dump.Documents
-	for i := range docs {
-		docs[i].ID = i
-	}
-	return docs, nil
-}
 
 // 単純な文字列一致で検索
 func search(docs []document, term string) []document {
